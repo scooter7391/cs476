b1f2c5d9e15c65d5fec089ae2dbfb24efc79d3fe
Scott Rupprecht <scooter7391@gmail.com>
2013-09-30 16:56:52 -0500

    Second version

--- Main.java
+++ Main.java
@@ -8,26 +8,31 @@ public class MainRupprecht {
 	 */
 	public static void main(String[] args) {
 			int random = new Random().nextInt(100);
+			int guess = 0;
 			System.out.println("Try to guess the number between 0 and 99. Type 100 to quit");
 			Scanner sc = new Scanner(System.in);
+		do
+		{
 			while (!sc.hasNext()) {
 
 			}
 			try {
-				int guess = Integer.parseInt(sc.nextLine());
+				guess = Integer.parseInt(sc.nextLine());
 				if (guess == random) {
 					System.out.println("Correct!");
-				} else if (guess > random) {
+				} else if (guess > random && guess != 100) {
 
 					System.out.println("Sorry too high, the number was "
 							+ random + ".");
-				} else {
+				} else if(guess != 100){
 					System.out.println("Sorry too low, the number was "
 							+ random + ".");
 				}
 			} catch (NumberFormatException e) {
 				System.err.println("Input was not a number");
 			}
+		}
+		while(guess != 100)
 			
 		
 		
